28fff4442307271b9b55c4e096326b555520b6d0
eddy <eddyjj92@gmail.com>
2026-06-02 10:53:34 -0400

    feat: fase 5-6 - middleware real, facade y render básico

--- middleware/inertia.go
+++ middleware/inertia.go
@@ -1,9 +1,29 @@
 package middleware
 
-import contractshttp "github.com/goravel/framework/contracts/http"
+import (
+	stdhttp "net/http"
 
-func Inertia() contractshttp.Middleware {
+	contractshttp "github.com/goravel/framework/contracts/http"
+
+	"github.com/eddyjj92/goravel-inertia/facades"
+)
+
+func Inertia(url string) contractshttp.Middleware {
 	return func(ctx contractshttp.Context) {
+		if ctx.Request().Header("X-Inertia") == "" {
+			ctx.Request().Next()
+			return
+		}
+
+		version := facades.Inertia().Version()
+		if ctx.Request().Method() == "GET" && ctx.Request().Header("X-Inertia-Version") != version {
+			w := ctx.Response().Writer()
+			w.Header().Set("X-Inertia-Location", url+ctx.Request().FullUrl())
+			w.WriteHeader(stdhttp.StatusConflict)
+			ctx.Request().Abort(stdhttp.StatusConflict)
+			return
+		}
+
 		ctx.Request().Next()
 	}
 }
