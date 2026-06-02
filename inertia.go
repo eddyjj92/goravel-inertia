9b0ba5040b43cf832c24f9bb56183b339abb56b4
eddy <eddyjj92@gmail.com>
2026-06-02 10:49:52 -0400

    feat: fase 3-4 - manager y service provider

--- inertia.go
+++ inertia.go
@@ -1,1 +1,72 @@
 package goravelinertia
+
+import (
+	"sync"
+
+	contractshttp "github.com/goravel/framework/contracts/http"
+)
+
+type InertiaManager struct {
+	mu           sync.RWMutex
+	adapter      *Adapter
+	version      string
+	sharedFuncs  map[string]func(contractshttp.Context) any
+}
+
+func NewInertiaManager(adapter *Adapter, version string) *InertiaManager {
+	return &InertiaManager{
+		adapter:     adapter,
+		version:     version,
+		sharedFuncs: make(map[string]func(contractshttp.Context) any),
+	}
+}
+
+func (m *InertiaManager) Render(ctx contractshttp.Context, component string, props map[string]any) error {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	evaluated := make(map[string]any, len(m.sharedFuncs))
+	for key, fn := range m.sharedFuncs {
+		evaluated[key] = fn(ctx)
+	}
+
+	for k, v := range props {
+		evaluated[k] = v
+	}
+
+	w := m.adapter.Writer(ctx)
+	r := m.adapter.Request(ctx)
+
+	return m.adapter.Inertia().Render(w, r, component, evaluated)
+}
+
+func (m *InertiaManager) Share(key string, value any) {
+	m.adapter.Inertia().Share(key, value)
+}
+
+func (m *InertiaManager) ShareFunc(key string, fn func(contractshttp.Context) any) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.sharedFuncs[key] = fn
+}
+
+func (m *InertiaManager) Location(ctx contractshttp.Context, url string) error {
+	w := m.adapter.Writer(ctx)
+	r := m.adapter.Request(ctx)
+
+	m.adapter.Inertia().Location(w, r, url)
+
+	return nil
+}
+
+func (m *InertiaManager) Version() string {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	return m.version
+}
+
+func (m *InertiaManager) GetAdapter() *Adapter {
+	return m.adapter
+}
--- providers/inertia_service_provider.go
+++ providers/inertia_service_provider.go
@@ -2,17 +2,52 @@ package providers
 
 import (
 	"github.com/goravel/framework/contracts/foundation"
+
+	goravelinertia "github.com/eddyjj92/goravel-inertia"
+	"github.com/eddyjj92/goravel-inertia/contracts"
+	"github.com/eddyjj92/goravel-inertia/facades"
+	petaki "github.com/petaki/inertia-go"
 )
 
 type InertiaServiceProvider struct {
 }
 
 func (p *InertiaServiceProvider) Register(app foundation.Application) {
+	app.Singleton("goravel.inertia", func(app foundation.Application) (any, error) {
+		config := app.MakeConfig()
+
+		rootView := "app"
+		version := ""
+		ssr := false
+		ssrURL := "http://127.0.0.1:13714/render"
+		url := ""
+
+		if config != nil {
+			rootView = config.GetString("inertia.root_view", "app")
+			version = config.GetString("inertia.version", "")
+			ssr = config.GetBool("inertia.ssr", false)
+			ssrURL = config.GetString("inertia.ssr_url", "http://127.0.0.1:13714/render")
+			url = config.GetString("app.url", "")
+		}
+
+		inertia := petaki.New(url, rootView, version)
+
+		if ssr {
+			inertia.EnableSsr(ssrURL)
+		}
+
+		adapter := goravelinertia.NewAdapter(inertia)
+		manager := goravelinertia.NewInertiaManager(adapter, version)
+
+		return manager, nil
+	})
 }
 
 func (p *InertiaServiceProvider) Boot(app foundation.Application) {
-}
+	instance, err := app.Make("goravel.inertia")
+	if err != nil {
+		panic("Failed to resolve goravel.inertia: " + err.Error())
+	}
 
-func (p *InertiaServiceProvider) Start(app foundation.Application) error {
-	return nil
+	facades.RegisterInertia(instance.(contracts.Inertia))
 }
